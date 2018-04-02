b455b9413e68ff9ca1d0df51d8033d45a6dded9b
ismurray <ians.murray@yahoo.com>
2018-04-02 15:00:54 -0400

    Establish animation frames
    - Define drawing loop
    - Making ball move
    - Clearing and redrawing canvas on each frame

--- assets/scripts/canvas.js
+++ assets/scripts/canvas.js
@@ -1,28 +1,28 @@
 'use strict'
 
-// const canvas = document.getElementById('myCanvas')
-// const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('myCanvas')
+const ctx = canvas.getContext('2d')
 
-const draw = function () {
-  const ctx = document.getElementById('myCanvas').getContext('2d')
+let x = canvas.width / 2
+let y = canvas.height - 30
+const dx = 2
+const dy = -2
+
+const drawBall = function () {
   ctx.beginPath()
-  ctx.rect(20, 40, 50, 50)
-  ctx.fillStyle = '#FF0000'
+  ctx.arc(x, y, 10, 0, Math.PI * 2)
+  ctx.fillStyle = '#0095DD'
   ctx.fill()
-  ctx.closePath()
-
-  ctx.beginPath()
-  ctx.arc(240, 160, 20, 0, Math.PI * 2, false)
-  ctx.fillStyle = 'green'
-  ctx.fill()
-  ctx.closePath()
-
-  ctx.beginPath()
-  ctx.rect(160, 10, 100, 40)
-  ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)'
-  ctx.stroke()
   ctx.closePath()
 }
+
+const draw = function () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBall()
+  x += dx
+  y += dy
+}
+setInterval(draw, 10)
 
 module.exports = {
   draw

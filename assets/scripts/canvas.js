8c00e354d1615dc19946025ae77c52f4cd0feb9a
ismurray <ians.murray@yahoo.com>
2018-04-02 11:35:20 -0400

    Draw cooler smiley :D
    Draw cooler smiley :D

--- assets/scripts/canvas.js
+++ assets/scripts/canvas.js
@@ -5,8 +5,8 @@ const ctx = canvas.getContext('2d')
 
 ctx.beginPath()
 ctx.arc(75, 75, 50, 0, Math.PI * 2, true) // Outer circle
-ctx.moveTo(110, 75)
-ctx.arc(75, 75, 35, 0, Math.PI, false)  // Mouth (clockwise)
+ctx.moveTo(60, 75)
+ctx.arc(75, 75, 15, 0, Math.PI, false)  // Mouth (clockwise)
 ctx.moveTo(65, 65)
 ctx.arc(60, 65, 5, 0, Math.PI * 2, true)  // Left eye
 ctx.moveTo(95, 65)

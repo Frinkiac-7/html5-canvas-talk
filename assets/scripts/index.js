f1730253c665d1ad212c358e19d64a10f644b499
ismurray <ians.murray@yahoo.com>
2018-04-02 11:51:25 -0400

    Draw some arcs
    Draw some arcs

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,11 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const canvas = require('./canvas')
 
 $(() => {
   setAPIOrigin(location, config)
-  require('./canvas')
+  canvas.draw()
 })
 
 // use require with a reference to bundle the file and use it in this file
